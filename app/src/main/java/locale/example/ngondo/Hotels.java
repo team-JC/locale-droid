77603748654621bc983be87d204f64f4dcc4d59b
Cornellius Ngondo <devngondo@gmail.com>
2015-05-19 15:30:44 +0000

    Modified splash screen

--- app/src/main/java/locale/example/ngondo/Hotels.java
+++ app/src/main/java/locale/example/ngondo/Hotels.java
@@ -1,38 +1,33 @@
 package locale.example.ngondo;
 
+import android.app.Activity;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Toast;
+
+import com.github.clans.fab.FloatingActionButton;
 
 
-public class Hotels extends ActionBarActivity {
+public class Hotels extends Activity {
+    private FloatingActionButton fab;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_hotels);
-    }
+        //Instantiate FAB
+        fab= (FloatingActionButton) findViewById(R.id.fab);
+        //Add listeners to FAB
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(Hotels.this,"FAB is working",Toast.LENGTH_SHORT).show();
+            }
+        });
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_hotels, menu);
-        return true;
-    }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
     }
 }
